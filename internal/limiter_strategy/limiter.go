c49d63faa7b70882e603c1fe9b4b3ad2173bbbe6
Evandro Junior <evandrojr@gmail.com>
2025-03-18 15:58:32 -0300

    Quebrado 2

--- internal/limiter_strategy/limiter.go
+++ internal/limiter_strategy/limiter.go
@@ -24,7 +24,10 @@ type acessos struct {
 	Tokens map[string]acessoRecord
 }
 
-func Init(configs configs.Config) {
+var envConfig configs.EnvConfig
+
+func Init(configs configs.EnvConfig) {
+	envConfig = configs
 	Acessos = acessos{
 		Ip:     make(map[string]acessoRecord),
 		Tokens: make(map[string]acessoRecord),
@@ -33,7 +36,27 @@ func Init(configs configs.Config) {
 }
 
 func BloquearIp(ip string) {
+	Acessos.Ip[ip] = acessoRecord{
+		NumeroAcessosNoSegundo: 0,
+		BloqueadoAte:           time.Now().Add(time.Second * time.Duration(envConfig.BlockIpTime)),
+	}
+}
 
+func BloquearToken(token string) {
+	Acessos.Tokens[token] = acessoRecord{
+		NumeroAcessosNoSegundo: 0,
+		BloqueadoAte:           time.Now().Add(time.Second * time.Duration(envConfig.BlockTokenTime)),
+	}
+}
+
+func VerificaBloqueio(segundo int64, ip string, token string) error {
+	if Acessos.Ip[ip].BloqueadoAte.After(time.Now()) {
+		return errors.New("IP bloqueado até " + Acessos.Ip[ip].BloqueadoAte.String())
+	}
+	if Acessos.Tokens[token].BloqueadoAte.After(time.Now()) {
+		return errors.New("Token bloqueado até " + Acessos.Tokens[token].BloqueadoAte.String())
+	}
+	return nil
 }
 
 func (l LimiterStrategyStruct) ValidaAcesso(segundoRegistrado int64, ip string, token string) error {
@@ -41,6 +64,9 @@ func (l LimiterStrategyStruct) ValidaAcesso(segundoRegistrado int64, ip string, 
 }
 
 func ValidaAcesso(segundo int64, ip string, token string) error {
+	if err := VerificaBloqueio(segundo, ip, token); err != nil {
+	}
+
 	RegistraAcessoTokenErr := RegistraAcessoToken(segundo, token)
 	if RegistraAcessoTokenErr != nil && RegistraAcessoTokenErr.Error() == tokenNotFound {
 		return RegistraAcessoIp(segundo, ip)
@@ -51,18 +77,18 @@ func ValidaAcesso(segundo int64, ip string, token string) error {
 func RegistraAcessoIp(segundo int64, ip string) error {
 	if segundo != segundoRegistrado {
 		segundoRegistrado = segundo
-		Acessos.Ip = make(map[string]int)
+		Acessos.Ip = make(map[string]acessoRecord)
 	}
 	println("Acessos.Ip[ip]", Acessos.Ip[ip])
-	if Acessos.Ip[ip] >= configs.Config.IpMaxReqPerSecond {
+	if Acessos.Ip[ip].NumeroAcessosNoSegundo >= envConfig.IpMaxReqPerSecond {
 		return errors.New(exceedIpLimit + ip)
 	}
-	Acessos.Ip[ip]++
+	Acessos.Ip[ip].++
 	return nil
 }
 
 func RegistraAcessoToken(segundo int64, token string) error {
-	if configs.Config.TokensMaxReqPerSecond[token] == 0 {
+	if envConfig.TokensMaxReqPerSecond[token] == 0 {
 		log.Println(tokenNotFound)
 		return errors.New(tokenNotFound)
 	}
@@ -70,7 +96,7 @@ func RegistraAcessoToken(segundo int64, token string) error {
 		segundoRegistrado = segundo
 		Acessos.Tokens = make(map[string]int)
 	}
-	if Acessos.Tokens[token] >= configs.Config.TokensMaxReqPerSecond[token] {
+	if Acessos.Tokens[token] >= envConfig.TokensMaxReqPerSecond[token] {
 		return errors.New(exceedTokenLimit + token)
 	}
 	Acessos.Tokens[token]++
